e5584b96777d24b9d772bce2127d157b1a2d5bf6
Bernardo Watanabe Venzi <bernardowvenzi@gmail.com>
2025-10-22 01:13:01 -0300

    feat: setup inicial do ambiente frontend com App Router

---
+++ gorgonas-frontend/src/app/page.tsx
@@ -0,0 +1,7 @@
+// src/app/page.tsx
+import FeedDeslogadoPage from "./feed-deslogado/page";
+
+// A rota raiz (/) renderiza o Feed Deslogado
+export default function HomePage() {
+  return <FeedDeslogadoPage />;
+}
