06e4eb13dc5545ccce12048357028d523fc0ab83
Andre Henrique <andrehsb06@gmail.com>
2025-11-20 20:41:39 -0300

    fix: Corrige rota do botão de voltar

--- gorgonas-frontend/src/app/(app)/produto/[id]/page.tsx
+++ gorgonas-frontend/src/app/(app)/produto/[id]/page.tsx
@@ -1,6 +1,7 @@
     'use client';
 
     import { useEffect, useState } from "react";
+    import { useRouter } from 'next/navigation';
     import { Navbar } from '@/components/Navbar';
     import { ProductCard } from '@/components/ProductCard';
     import { useParams } from "next/navigation";
@@ -31,6 +32,7 @@
 
 
     export default function ProductPage() {
+        const router = useRouter();
         const [products, setProducts] = useState<Products | null>(null);
         const { id } = useParams();
         const [selectedImageUrl, setSelectedImageUrl] = useState<string | null>(null);
@@ -111,10 +113,10 @@
                 <Navbar />
                 <div className="max-w-7xl mx-auto p-4 md:p-8">
 
-                    <a href="/" className="flex items-center gap-1 text-gray-600 hover:text-black mb-4 w-fit">
+                    <button onClick={() => router.back()} className="flex items-center gap-1 text-gray-600 hover:text-black mb-4 w-fit">
                         <BackArrowIcon />
                         <span className="font-medium">Voltar</span>
-                    </a>
+                    </button>
 
                     {/* --- 1. SEÇÃO PRINCIPAL */}
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-50">
