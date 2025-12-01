58cd7fcdb3413b446037e3844b551b835b20ab89
Pedro Americo <dev.americo@gmail.com>
2025-12-01 18:04:12 -0300

    feat: head de perfil

--- gorgonas-frontend/src/app/(app)/perfil/[id]/page.tsx
+++ gorgonas-frontend/src/app/(app)/perfil/[id]/page.tsx
@@ -5,9 +5,11 @@ import ProductScroll from '@/components/ProductScroll';
 import { useParams, useRouter } from 'next/navigation';
 import api from '@/utilis/api';
 import { useAuth } from '@/contexts/AuthContext';
-import { useState, useEffect } from "react";
+import { useState, useEffect, use } from "react";
 import { toast } from "react-toastify";
 import { ProductRow } from "@/components/ProductRow";
+import { ProfileHeader } from "@/components/headPerfil";
+import { ProfileHeaderProps } from "@/components/headPerfil";
 
 type ProdutoParaCard = {
     id: number;
@@ -23,6 +25,7 @@ export default function PerfilPage() {
   const router = useRouter();
   const { user } = useAuth();
   const [isLoading, setIsLoading] = useState(false);
+  const [perfil, setPerfil] = useState<ProfileHeaderProps | null>(null);
 
   const [produtos, setProdutos] = useState<ProdutoParaCard[]>([]);
 
@@ -53,10 +56,33 @@ export default function PerfilPage() {
         listarProdutos();
 }, [idPerfil.id]);
 
+
+useEffect(() => {
+    
+    setIsLoading(true);
+
+    async function procurarPerfil() {
+
+    try {
+        const response = await api.get(`/usuario/${idPerfil.id}`);
+        setPerfil(response.data);
+    } catch (error) {
+        toast.error("Erro ao carregar perfil do usuário.");
+    } finally {
+        setIsLoading(false);
+    }
+    }
+    procurarPerfil();
+}, [idPerfil.id]);
+
 return (
     <main className="bg-[#FDF9F2] min-h-screen">
                 <Navbar />
+                <ProfileHeader
+                    perfil={perfil}
+                />
         <div className="max-w-7xl mx-auto p-4 md:p-8">
+        
         <ProductRow 
             title="Produtos"
             products={produtos}
